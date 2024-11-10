c6aa663d3b1ea7d8f12b1c5e357ed1cabd176bf5
rifrifqyqy <rifqyh22@gmail.com>
2024-11-10 14:23:25 +0700

    auth add debugging log

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "utils/db.server";
-import { json, redirect } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { useActionData, Form } from "@remix-run/react";
 import bcrypt from "bcryptjs";
 import { createSession } from "utils/session.server";
@@ -13,20 +13,26 @@ export async function action({ request }) {
   const username = formData.get("username");
   const password = formData.get("password");
 
-  // Cari user berdasarkan username
-  const user = await prisma.user.findUnique({ where: { username } });
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return json({ error: "Username atau password salah" }, { status: 400 });
+  try {
+    // Cari user berdasarkan username
+    const user = await prisma.user.findUnique({ where: { username } });
+    console.log("user found", user);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return json({ error: "Username atau password salah" }, { status: 400 });
+    }
+
+    // Update `lastLogin`
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { lastLogin: new Date() },
+    });
+
+    // Buat sesi login
+    return createSession(user.id, "/");
+  } catch (error) {
+    console.error(error);
+    return json({ error: "Terjadi kesalahan pada server." }, { status: 500 });
   }
-
-  // Update `lastLogin`
-  await prisma.user.update({
-    where: { id: user.id },
-    data: { lastLogin: new Date() },
-  });
-
-  // Buat sesi login
-  return createSession(user.id, "/");
 }
 
 export default function Login() {
