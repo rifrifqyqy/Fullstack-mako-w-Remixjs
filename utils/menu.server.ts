a8ab17fab34b18c6e9a7c459db30f84c9c7620a9
rifrifqyqy <rifqyh22@gmail.com>
2024-11-15 19:47:39 +0700

    commented code get function

--- utils/menu.server.ts
+++ utils/menu.server.ts
@@ -1,17 +1,17 @@
 import { prisma } from "./db.server";
 
-// fungsi mengambil semua data menu dari database
+// :GET  semua data menu dari database
 export async function getAllMenu() {
   return await prisma.menu.findMany();
 }
-// fungsi mengambil data menu berdasarkan id
+// :GET(:id) data menu berdasarkan id
 export async function getMenu(id: string) {
   return await prisma.menu.findUnique({
     where: { id },
   });
 }
 
-// fungsi mengambil data menu terbaru
+// :GET(updated: at)  data menu terbaru
 export async function getLatestMenu() {
   return await prisma.menu.findFirst({
     orderBy: { id: "desc" },
