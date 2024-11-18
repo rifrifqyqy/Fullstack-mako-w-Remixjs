8be1cce696626114ef1325ac105c9f87cbfc396a
rifrifqyqy <rifqyh22@gmail.com>
2024-11-18 09:13:04 +0700

    change gif loading

--- app/components/Fragments/LoadingModal.tsx
+++ app/components/Fragments/LoadingModal.tsx
@@ -1,15 +1,17 @@
 // app/components/LoadingModal.tsx
 
-export default function LoadingModal() {
+export default function LoadingModal({ title }: { title: string }) {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-50">
       <div className="rounded-2xl bg-white p-6 shadow-lg">
-        <div className="text-center">
-          <iframe
-            src="https://lottie.host/embed/acf744ff-2337-49b9-b6cd-59232a6e69ef/41Awo0aEoT.lottie"
+        <div className="flex flex-col items-center justify-center text-center">
+          <img
+            src="gif/load.gif"
             title="loading"
-          ></iframe>
-          <p className="text-lg text-gray-700">Sedang menyimpan data...</p>
+            className="h-32"
+            alt="loading"
+          ></img>
+          <p className="text-lg text-gray-700">{title}</p>
         </div>
       </div>
     </div>
