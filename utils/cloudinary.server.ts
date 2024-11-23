c29c6f8f17582f4daeb46faf8af8f56aa63a6153
rifrifqyqy <rifqyh22@gmail.com>
2024-11-23 21:36:21 +0700

    change upload data method add publicthumb and gallery public id | add delete data method

--- utils/cloudinary.server.ts
+++ utils/cloudinary.server.ts
@@ -1,4 +1,9 @@
-export async function uploadToCloudinary(file: File): Promise<string> {
+import crypto from "crypto";
+
+// post image to cloudinary
+export async function uploadToCloudinary(
+  file: File,
+): Promise<{ url: string; publicId: string }> {
   const data = new FormData();
   data.append("file", file);
   data.append("upload_preset", process.env.CLOUDINARY_UPLOAD_PRESET as string);
@@ -9,5 +14,34 @@ export async function uploadToCloudinary(file: File): Promise<string> {
   });
 
   const json = await response.json();
-  return json.secure_url;
+  return { url: json.secure_url, publicId: json.public_id };
+}
+
+
+// delete image cloudinary
+export async function deleteFromCloudinary(publicId: string): Promise<boolean> {
+  const timestamp = Math.round(new Date().getTime() / 1000);
+  const apiSecret = process.env.CLOUDINARY_API_SECRET as string;
+  const signature = crypto
+    .createHash("sha256")
+    .update(`public_id=${publicId}&timestamp=${timestamp}${apiSecret}`)
+    .digest("hex");
+
+  const data = new URLSearchParams({
+    public_id: publicId,
+    timestamp: timestamp.toString(),
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    signature,
+  });
+
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/destroy`,
+    {
+      method: "POST",
+      body: data,
+    },
+  );
+
+  const result = await response.json();
+  return result.result === "ok";
 }
