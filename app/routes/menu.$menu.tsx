ddff3926915a97d8dd47a73fa6397166d27b583e
rifrifqyqy <rifqyh22@gmail.com>
2024-11-29 22:29:29 +0700

    separate cardReview

--- app/routes/menu.$menu.tsx
+++ app/routes/menu.$menu.tsx
@@ -20,6 +20,7 @@ import DeleteReview from "~/components/Fragments/form/DeleteReview";
 import AddReviewForm from "~/components/Fragments/form/AddReview";
 import formatDate from "helper/formatDate";
 import RemixFooter from "~/components/Layouts/RemixFooter";
+import CardReview from "~/components/Fragments/Card/CardReview";
 
 // ================== BACKEND LOGIC ==========================
 
@@ -333,77 +334,18 @@ export default function MenuDetail() {
                       Ulasan Anda
                     </p>
                     {/* your comment card */}
-                    <figure className="flex w-full flex-col rounded-2xl border-2 border-zinc-300 bg-white p-4">
-                      <section className="header-comment flex items-start gap-4">
-                        <div className="rounded-full bg-gray-200 p-2">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="32"
-                            height="32"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              fill="currentColor"
-                              d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"
-                            />
-                          </svg>
-                        </div>
-                        <article className="flex w-full flex-col gap-2">
-                          <header className="flex w-full justify-between">
-                            <p className="text-lg font-semibold capitalize">
-                              {currentUser?.username}
-                              <p className="text-sm font-normal text-zinc-500">
-                                {formatDate(userReview?.updatedAt, "date")}
-                              </p>
-                            </p>
-                            <div className="star flex h-fit items-center gap-2 rounded-full bg-zinc-200 py-1 pl-2 pr-3 font-semibold">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                className="text-amber-500"
-                              >
-                                <path
-                                  fill="currentColor"
-                                  d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.62L12 2L9.19 8.62L2 9.24l5.45 4.73L5.82 21z"
-                                />
-                              </svg>
-                              <p>{userReview?.rating}</p>
-                            </div>
-                          </header>
-                          {isEditing ? (
-                            <EditReviewForm
-                              userReview={userReview}
-                              onSubmit={() => setIsEditing(!isEditing)}
-                            />
-                          ) : (
-                            <p>{userReview?.comment}</p>
-                          )}
-
-                          {/* Tombol Edit */}
-                          <div className="flex items-center gap-2">
-                            <button
-                              type="button"
-                              onClick={() => setIsEditing(!isEditing)}
-                              className="text-sm text-zinc-600 hover:text-zinc-900"
-                            >
-                              {isEditing ? "Batal Edit" : "Edit"}
-                            </button>
-                            <span className="text-zinc-500">|</span>
-                            {/* Form untuk Menghapus Review */}
-                            <DeleteReview reviewId={userReview?.id} />
-                          </div>
-                        </article>
-                      </section>
-                      <section className="body-comment"></section>
-                    </figure>
+                    <CardReview
+                      currentUser={currentUser}
+                      userReview={userReview}
+                      actionFetcher={`/menu/${menuData.id}`}
+                      type="yourReview"
+                    />
                     {/* end your comment card */}
                     {/* Form untuk Mengedit Review */}
                   </div>
                 ) : currentUser && currentUser.id ? (
                   <main className="mt-4">
-                    <AddReviewForm />
+                    <AddReviewForm actionFetcher={`/menu/${menuData.id}`} />
                   </main>
                 ) : (
                   <main className="mt-4 flex flex-col items-center justify-center gap-4 rounded-2xl border-2 border-zinc-200 px-4 py-6">
@@ -430,52 +372,14 @@ export default function MenuDetail() {
                 </h1>
                 {reviews.map((review) => (
                   // start card
-                  <figure
-                    className="flex w-full flex-col rounded-2xl border-2 border-zinc-300 bg-white p-4"
+                  <CardReview
+                    type="generalReview"
                     key={review.id}
-                  >
-                    <section className="header-comment flex items-start gap-4">
-                      <div className="rounded-full bg-gray-200 p-2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="32"
-                          height="32"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"
-                          />
-                        </svg>
-                      </div>
-                      <article className="flex w-full flex-col gap-2">
-                        <header className="flex w-full justify-between">
-                          <p className="text-lg font-semibold capitalize">
-                            {review.username}
-                            <p className="text-sm font-normal text-zinc-500">
-                              {formatDate(review?.updatedAt, "date")}
-                            </p>
-                          </p>
-                          <div className="star flex h-fit items-center gap-2 rounded-full bg-zinc-200 py-1 pl-2 pr-3 font-semibold">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="20"
-                              height="20"
-                              viewBox="0 0 24 24"
-                              className="text-amber-500"
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.62L12 2L9.19 8.62L2 9.24l5.45 4.73L5.82 21z"
-                              />
-                            </svg>
-                            <p>{review?.rating}</p>
-                          </div>
-                        </header>
-                        <p>{review.comment}</p>
-                      </article>
-                    </section>
-                  </figure>
+                    username={review.username}
+                    displayReview={review.comment}
+                    dateReview={review.createdAt}
+                    rating={review.rating}
+                  />
 
                   // end card
                 ))}
