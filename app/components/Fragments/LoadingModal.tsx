5c119e9f7c924db90aac13151a13d9f2c275e474
rifrifqyqy <rifqyh22@gmail.com>
2024-12-11 21:15:43 +0700

    style: responsive mobile modal

--- app/components/Fragments/LoadingModal.tsx
+++ app/components/Fragments/LoadingModal.tsx
@@ -3,15 +3,15 @@
 export default function LoadingModal({ title }: { title: string }) {
   return (
     <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-50">
-      <div className="rounded-2xl bg-white px-16 py-8 shadow-lg">
+      <div className="rounded-2xl bg-white px-12 py-4 shadow-lg md:px-16 md:py-8">
         <div className="flex flex-col items-center justify-center gap-8 text-center">
           <img
             src="/gif/breadbutter2.gif"
             title="loading"
-            className="h-32"
+            className="h-24 md:h-32"
             alt="loading"
           ></img>
-          <p className="text-lg text-gray-700">{title}</p>
+          <p className="text-sm text-gray-700 md:text-lg">{title}</p>
         </div>
       </div>
     </div>
