c1cfc91bbbca844a8c7584133544519787aa4eab
rifrifqyqy <rifqyh22@gmail.com>
2024-12-29 16:50:30 +0700

    fix: button login issue didn't working on mobile device

--- app/components/Fragments/RemixNavbar.tsx
+++ app/components/Fragments/RemixNavbar.tsx
@@ -431,24 +431,20 @@ export function RemixNavbarHome({
           to={btn_to}
           title={btn_title}
           color={color}
-          onClick={handleClick}
-          stylebtn="flex mt-8 w-full text-center justify-center max-md:font-medium "
+          stylebtn={`${btn_to === "/logout" ? "" : " cursor-pointer "} w-full flex justify-center mt-8 text-center`}
         >
           {userIcon && (
-            <div className="mx-auto flex items-center gap-4 font-medium">
-              <h1>{btn_title_mobile}</h1>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1.3em"
-                height="1.3em"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="m17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5M4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4z"
-                />
-              </svg>
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"
+              />
+            </svg>
           )}
         </RemixButton>
       </main>
